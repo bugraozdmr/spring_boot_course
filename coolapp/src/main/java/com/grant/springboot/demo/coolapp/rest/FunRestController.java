60ba0fe5a65377d70a2f26065e3f21242b726651
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2024-11-30 19:23:29 +0300

    first commit

---
+++ coolapp/src/main/java/com/grant/springboot/demo/coolapp/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.grant.springboot.demo.coolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose "/" than return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Work every day";
+    }
+}
