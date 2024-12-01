03701da677d8a53396434fd78d2f77db681f752c
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2024-12-01 10:50:06 +0300

    quick start

--- coolapp/src/main/java/com/grant/springboot/demo/coolapp/rest/FunRestController.java
+++ coolapp/src/main/java/com/grant/springboot/demo/coolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.grant.springboot.demo.coolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    // expose team info endpoint
+
+    @GetMapping("/team-info")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + "<br>Team Name: " + teamName;
+    }
+
     // expose "/" than return "Hello World"
 
     @GetMapping("/")
